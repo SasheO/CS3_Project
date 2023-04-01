647c00a55e64faed8ac8a505b497760875cf2b73
SasheO <76496417+SasheO@users.noreply.github.com>
2023-04-01 14:12:39 -0400

    added comments and more tests

--- card_no.py
+++ card_no.py
@@ -1,5 +1,13 @@
 def valid_card_no(card_no):
-    # https://www.geeksforgeeks.org/luhn-algorithm/
+    '''
+    detects whether a credit card number is valid using luhn's algorithm
+    
+    input: 
+        card_no: (int) the card number
+
+    output:
+        bool: if the card number is valid (True) or invalid (False)
+    '''
     if type(card_no) != str:
         card_no = str(card_no).strip()
     
@@ -15,7 +23,14 @@ def valid_card_no(card_no):
             summ += int(card_no[indx])
     return summ%10 == 0
 
+
 if __name__ == "__main__":
     assert valid_card_no(49927398716) == True, "49927398716 = True"
     assert valid_card_no(49927398717) == False, "49927398717 = False"
     assert valid_card_no(1234567812345620) == False, "1234567812345620 = False"
+    assert valid_card_no(1234567890) == False, "1234567890 = False"
+    assert valid_card_no(1234567812345678) == False, "1234567812345678 = False"
+    assert valid_card_no(61789372994) == True, "61789372994 = True"
+    assert valid_card_no(5146716835430) == True, "5146716835430 = True"
+    assert valid_card_no(5146716835431) == False, "5146716835431 = False"
+    assert valid_card_no(4661074345770850) == True, "4661074345770850 = True"
