7bda5890bec5e695cea6adb99f492b7847e19ff2
Malcolm <41296369+Xaiver71@users.noreply.github.com>
2023-04-06 20:52:34 -0400

    Modified it to be a function and as well customizable length

--- token_generator.py
+++ token_generator.py
@@ -1,9 +1,12 @@
 import string
 import secrets
-alphenumeric = string.ascii_letters + string.digits
-while True:
-    token = ''.join(secrets.choice(alphenumeric) for i in range(10))
-    if (any(c.islower() for c in token)
-            and any(c.isupper() for c in token)
-            and sum(c.isdigit() for c in token) >= 3):
-        break
+
+def token_generator():
+    token_length = int(input("Enter the length of token"))
+    alphenumeric = string.ascii_letters + string.digits
+    while True:
+        token = ''.join(secrets.choice(alphenumeric) for i in range(token_length))
+        if (any(c.islower() for c in token)
+                and any(c.isupper() for c in token)
+                and sum(c.isdigit() for c in token) >= 3):
+            break
