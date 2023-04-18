a5ce65a81e1a2090f9e12330609f48b96d146a67
SasheO <76496417+SasheO@users.noreply.github.com>
2023-04-18 17:52:38 -0400

    included @Xaiver71 's token generator function
    there is a merge issue from the pull request so i just manually moved the function

--- dummy_package_name/dummy_package_name/dummy_package_name.py
+++ dummy_package_name/dummy_package_name/dummy_package_name.py
@@ -45,3 +45,12 @@ def valid_card_no(card_no):
         else:
             summ += int(card_no[indx])
     return summ%10 == 0
+
+def token_generator(token_length):
+    alphenumeric = string.ascii_letters + string.digits
+    while True:
+        token = ''.join(secrets.choice(alphenumeric) for i in range(token_length))
+        if (any(c.islower() for c in token)
+                and any(c.isupper() for c in token)
+                and sum(c.isdigit() for c in token) >= 3):
+            break
