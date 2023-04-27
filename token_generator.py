2e0eb32e1fb3d4f6ca7c5e75acfae60ab98b2c6e
Malcolm <41296369+Xaiver71@users.noreply.github.com>
2023-04-27 19:36:12 -0400

    Token_Generator-patch-3

--- token_generator.py
+++ token_generator.py
@@ -1,15 +1,13 @@
 import string
 import secrets
 
-def token_generator():
-    token_length = int(input("Enter the length of token"))
+def token_generator(token_length):
     alphenumeric = string.ascii_letters + string.digits
     while True:
         token = ''.join(secrets.choice(alphenumeric) for i in range(token_length))
         if (any(c.islower() for c in token)
                 and any(c.isupper() for c in token)
                 and sum(c.isdigit() for c in token) >= 3):
-            print(token)
             return(token)
             
   
